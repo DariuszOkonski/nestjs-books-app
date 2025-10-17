7483e0471da836861e89b7afabfb0c2521c7a91c
Dariusz Okoński <darek200180@gmail.com>
2025-10-17 07:54:07 +0200

    add getById

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 
 @Controller('authors')
@@ -8,5 +14,16 @@ export class AuthorsController {
   @Get('/')
   getAll() {
     return this.authorsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const author = await this.authorsService.getById(id);
+
+    if (!author) {
+      throw new NotFoundException('Author not found');
+    }
+
+    return author;
   }
 }
