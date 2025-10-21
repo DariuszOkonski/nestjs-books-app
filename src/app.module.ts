3915f980ce5c8737dabc96c1122f8e2f58ec4988
Dariusz Okoński <darek200180@gmail.com>
2025-10-21 08:27:58 +0200

    connect users module with prismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
 import cors from 'cors';
 
 @Module({
-  imports: [AuthorsModule, BooksModule],
+  imports: [AuthorsModule, BooksModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
