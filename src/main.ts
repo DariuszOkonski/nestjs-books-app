6163f5dda21b0f8644e68d73d9c179dbda5cb072
Dariusz Okoński <darek200180@gmail.com>
2025-10-22 08:48:04 +0200

    add and fix issue with cookie-parser in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
 
   await app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
