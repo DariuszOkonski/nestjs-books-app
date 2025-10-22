21fc038fa500a4dc0a301758aec92a5dd2f3e8e4
Dariusz Okoński <darek200180@gmail.com>
2025-10-22 06:53:14 +0200

    add controller getById for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -8,5 +14,16 @@ export class UsersController {
   @Get('/')
   getAll() {
     return this.usersService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = await this.usersService.getById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 }
