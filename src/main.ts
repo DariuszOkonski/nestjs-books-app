3676f734d226af8b2acfca4f0be2237976d56946
Dariusz Okoński <darek200180@gmail.com>
2025-10-23 07:37:35 +0200

    use ConfigService where required

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
   app.use(cookieParser());
 
   await app.enableShutdownHooks();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.get('port') || '3000');
 }
 bootstrap();
